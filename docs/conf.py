7fa8108b5d3cde7d9a85616a6c59783b65f1b680
Aniket Tekawade <atekawade@anl.gov>
2021-03-17 14:43:40 -0500

    fix conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,8 @@ release = version
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+bibtex_bibfiles = ['refs.bib']
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
@@ -46,10 +48,17 @@ extensions = [
     'sphinx.ext.viewcode', 
     'sphinxcontrib.bibtex']
 
-
-napoleon_google_docstring = False
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = False
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
 napoleon_use_param = False
-napoleon_use_ivar = True
+napoleon_use_rtype = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
